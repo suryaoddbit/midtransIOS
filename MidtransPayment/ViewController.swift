831486696d3abd8b3827e4d7d4b1980037443da0
I Wayan Surya Adi Yasa <surya@oddbit.id>
2017-04-12 10:00:54 +0800

    Initial Commit

---
+++ MidtransPayment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MidtransPayment
+//
+//  Created by I Wayan Surya Adi Yasa on 4/12/17.
+//  Copyright © 2017 ODDBIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
